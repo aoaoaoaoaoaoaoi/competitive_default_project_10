e84ae9982bd402257039973eb7e9c173108b9885
aoaoaoaoaoaoaoi <gummi.bear456aaaaa@gmail.com>
2021-06-25 23:40:25 +0900

    add library

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF /*1000000007*/ 1000000000000000003
+#define MOD 1000000007
+#define ll long long
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+const double PI = acos(-1);
+
+//printf("%.10f", );
+
+/***********************************************************************/
+
+int main() {
+  int n; cin >> n;
+  
+  int ans = 0;
+  cout << ans << endl;
+}
