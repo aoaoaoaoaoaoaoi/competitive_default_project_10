11b539e4c2a1ad2ddb8b5462ce3594bdf2b7d8f2
aoaoaoaoaoaoaoi <gummi.bear456aaaaa@gmail.com>
2021-07-05 00:37:23 +0900

    add warshallFloyd

--- code.cpp
+++ code.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF /*1000000007*/ 1000000000000000003
+#define INF 1000000007 /*1000000000000000003*/
 #define MOD 1000000007
 #define ll long long
 #define rep(i,n) for (int i = 0; i < (n); ++i)
@@ -9,12 +9,12 @@ using namespace std;
 const double PI = acos(-1);
 
 //printf("%.10f", );
+//vector<vector<int>> data(3, vector<int>(4, -1));
 
 /***********************************************************************/
 
 int main() {
-  int n; cin >> n;
-  
+  int n, m; cin >> n >> m;
   int ans = 0;
   cout << ans << endl;
 }
