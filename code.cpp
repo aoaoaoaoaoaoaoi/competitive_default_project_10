1b54d84d981db54a8e85f0945e23a8587001427a
aoaoaoaoaoaoaoi <gummi.bear456aaaaa@gmail.com>
2021-08-01 01:20:13 +0900

    0801

--- code.cpp
+++ code.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF 1000000007 /*1000000000000000003*/
+#define INF /*1000000007*/ 5000000000000000003
 #define MOD 1000000007
 #define ll long long
 #define rep(i,n) for (int i = 0; i < (n); ++i)
@@ -15,6 +15,39 @@ const double PI = acos(-1);
 
 int main() {
   int n, m; cin >> n >> m;
-  int ans = 0;
+  vector<ll>a(n);
+  rep(i, n){
+    cin >> a[i];
+  }
+  vector<ll>b(m);
+  rep(i, m){
+    cin >> b[i];
+  }
+  sort(a.begin(), a.end());
+  a.erase(unique(a.begin(), a.end()), a.end());
+  sort(b.begin(), b.end());
+  b.erase(unique(b.begin(), b.end()), b.end());
+
+  ll ans = INF;
+  rep(ai, a.size()){
+    int ok = -1;
+    int ng = b.size();
+    while(ng - ok > 1){
+      int mid = (ok + ng) / 2;
+      if(b[mid] <= a[ai]){
+        ok = mid;
+      }else{
+        ng = mid;
+      }
+    }
+    
+    if(0 <=ok && ok < b.size()){
+      ans = min(ans, abs(a[ai]-b[ok]));
+    }
+    if(ok < ((int)(b.size()) - 1)){
+      ans = min(ans, abs(a[ai]-b[ok+1]));
+    }
+  }
+
   cout << ans << endl;
 }
