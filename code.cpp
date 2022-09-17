a56f9635dbd8b72efd13966359e4f781390d7739
aoaoaoaoaoaoaoi <40077710+aoaoaoaoaoaoaoi@users.noreply.github.com>
2022-09-17 23:04:38 +0900

    20220917

--- code.cpp
+++ code.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF 1001001001
-#define LINF 1001001001001001001
-#define MOD 1000000007
-#define ll long long
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;//get<0>(tuple) かっこを忘れない
 
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const ll MOD = 1000000007;
 const double PI = acos(-1);
 
 //printf("%.10f", );
 //vector<vector<int>> data(n);
 //vector<vector<int>> data(3, vector<int>(4, -1));
-//priority_queue<ll, vector<ll>, greater<ll>>que; 小さい順の優先度つきキュー
+//vector x(n, vector<bool>(n))
+//priority_queue<ll, vector<ll>, greater<ll>> que; 小さい順の優先度つきキュー
 
 /***********************************************************************/
 
-int main() {
-  int l, q; cin >> l >> q;
-  
-  cout << ans << endl;
+int main(){
+    int n; cin >> n;
+
+    cout << ans << endl;
 }
